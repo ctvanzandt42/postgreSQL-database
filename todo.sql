b33f96c69c69e91229a362d116a652548624e951
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-08-29 10:50:10 -0500

    fixed the delete part

--- todo.sql
+++ todo.sql
@@ -1,4 +1,4 @@
-create table todos (
+create table todo (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     details TEXT NULL,
@@ -7,17 +7,17 @@ create table todos (
     completed_at VARCHAR(100) NULL
 );
 
-insert into todos (title, priority, created_at, completed_at)
+insert into todo (title, priority, created_at, completed_at)
 values ('Run for your life from Shia Labeouf', 1, '2017-08-28 13:23:35', '2017-08-28 14:41:10'),
 ('Wrestling a knife from Shia Labeouf', 2, '2017-08-29 21:23:05', null),
 ('Strangling superstar Shia Labeouf', 3, '2017-09-01 12:29:13', null),
 ('Stab it in his kidney', 4, '2017-09-01 12:20:19', null),
 ('Stop blood oozing from stump leg', 5, '2017-09-01 12:30:39', null);
 
-select * from todos where completed_at = null;
+select * from todo where completed_at = null;
 
-select * from todos where priority > 1;
+select * from todo where priority > 1;
 
-update todos SET completed_at = '2017-09-01 01:13:01' WHERE id = 2; 
+update todo SET completed_at = '2017-09-01 01:13:01' WHERE id = 2; 
 
-delete * from todos where completed_at != null;
+delete from todo where completed_at IS NOT NULL;
